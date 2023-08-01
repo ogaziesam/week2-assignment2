8f7dea64211613f70b36988575a7fa3871f34af8
ogaziesam <140881332+ogaziesam@users.noreply.github.com>
2023-08-01 01:16:56 +0100

    Create subtraction to be returned in the function

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// Create variables to accept first and second user input
-// Create a subtraction function
-// The subraction function will return the subtraction for input a and b
-// Invoke subtraction function to run the calculator
+//Create variables to accept first and second user input
+let a = parseInt(prompt("Insert current year"));
+let b = parseInt(prompt("Insert birth year"));
+
+//Create a subtraction function
+function subtraction(a, b){
+//The subraction function will return the subtraction for input a and b
+return a - b
+}
+
+//Invoke subtraction function to run the calculator
